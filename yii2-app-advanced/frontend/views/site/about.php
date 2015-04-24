c392f563f9e81a0a06565e1650fef7aae3116af5
githubjeka <et.coder@gmail.com>
2015-04-24 16:42:04 +0300

    translate

--- yii2-app-advanced/frontend/views/site/about.php
+++ yii2-app-advanced/frontend/views/site/about.php
@@ -8,7 +8,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+    <p>Это статическая страница, которая может быть изменена в файле:</p>
 
     <code><?= __FILE__ ?></code>
 </div>
